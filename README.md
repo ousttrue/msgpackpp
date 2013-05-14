f32c7623c43541ed174dfe90f007c109eb94f650
ousttrue <ousttrue@gmail.com>
2013-05-15 04:22:19 +0900

    update sample

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-msgpack-asiorpc
-===============
+msgpack-rpc-asio
+================
 msgpack-rpc
 
 https://github.com/msgpack/msgpack-rpc
@@ -8,5 +8,68 @@ https://github.com/msgpack/msgpack-rpc
 
 ToDo
 ----
-* 名前被りがつらいのでclass名(ucc), 関数名(lcc), 変数名(スネーク)で
+* namespaceをmsgpack::asiorpcからmsgpack::rpc::asioに変更する
 
+Install
+-------
+msgpack-rpcにinclude/msgpack/rpc/asio.hを追加する。
+msgpack-rpcのビルドは不要。
+include/msgpack/rpc/exception.h
+include/msgpack/rpc/protocol.h
+include/msgpack/rpc/types.h
+のみを使用する。
+
+Requied
+-------
+* msgpack
+* boost
+
+Sample
+------
+```c++
+#include <msgpack/asiorpc.h>
+#include <boost/thread.hpp>
+
+
+int main(int argc, char **argv)
+{
+    const static int PORT=8070;
+
+    // server
+    boost::asio::io_service server_io;
+    msgpack::asiorpc::server server(server_io);
+
+    // ToDo
+    std::function<int(int, int)> func1=[](int a, int b)->int{ return a+b; };
+    server.get_dispatcher()->add_handler(func1, "add");
+
+    // ToDo
+    std::function<float(float, float)> func2=[](float a, float b)->float{ return a*b; };
+    server.get_dispatcher()->add_handler(func2, "mul");
+
+    server.start(boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), PORT));
+    boost::thread server_thread([&server_io](){ server_io.run(); });
+
+    // client
+    boost::asio::io_service client_io;
+    auto client=msgpack::asiorpc::session::create(client_io); 
+    client->connect(boost::asio::ip::tcp::endpoint(
+                    boost::asio::ip::address::from_string("127.0.0.1"), PORT));
+    boost::thread clinet_thread([&client_io](){ client_io.run(); });
+
+    // request
+    auto request1=client->call(std::function<int(int, int)>(), "add", 1, 2);
+    auto request2=client->call(std::function<float(float, float)>(), "mul", 1.2f, 5.0f);
+
+    std::cout << request1->get_sync<int>() << std::endl;
+    std::cout << request2->get_sync<float>() << std::endl;
+
+    client_io.stop();
+    clinet_thread.join();
+
+    server_io.stop();
+    server_thread.join();
+
+    return 0;
+}
+```
--- sample/main.cpp
+++ sample/main.cpp
@@ -1,92 +1,40 @@
 #include <msgpack/asiorpc.h>
 #include <boost/thread.hpp>
 
-const static int PORT=8070;
-
-static int add(int a, int b)
-{
-    return a+b;
-}
-static int mul(int a, int b)
-{
-    return a*b;
-}
-
-class server
-{
-    boost::asio::io_service m_io_service;
-    msgpack::asiorpc::server m_server;
-    std::shared_ptr<boost::thread> m_thread;
-public:
-    server()
-        : m_server(m_io_service)
-    {
-    }
-
-    ~server()
-    {
-		stop();
-    }
-
-    void run()
-    {
-        m_server.start(boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), PORT));
-
-        // register callback
-        m_server.get_dispatcher()->add_handler(&add, "add");
-        m_server.get_dispatcher()->add_handler(&mul, "mul");
-
-        auto service=&m_io_service;
-        m_thread=std::make_shared<boost::thread>([service]{ service->run(); });
-
-        boost::this_thread::sleep( boost::posix_time::milliseconds(300));
-    }
-
-    void stop()
-    {
-		m_io_service.stop();
-		m_thread->join();
-    }
-};
-
-
-static int type_dummy(int, int)
-{
-    return 0;
-}
 
 int main(int argc, char **argv)
 {
-    server s;
-    s.run();
+    const static int PORT=8070;
 
-    // start client
-    {
-        boost::asio::io_service io_service;
-        auto endpoint=boost::asio::ip::tcp::endpoint(
-                    boost::asio::ip::address::from_string("127.0.0.1"), PORT);
-        auto client=msgpack::asiorpc::session::create(io_service); 
-        client->connect(endpoint);
+    // server
+    boost::asio::io_service server_io;
+    msgpack::asiorpc::server server(server_io);
+    std::function<int(int, int)> func1=[](int a, int b)->int{ return a+b; };
+    server.get_dispatcher()->add_handler(func1, "add");
+    std::function<float(float, float)> func2=[](float a, float b)->float{ return a*b; };
+    server.get_dispatcher()->add_handler(func2, "mul");
+    server.start(boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), PORT));
+    boost::thread server_thread([&server_io](){ server_io.run(); });
 
-		// run background
-		boost::thread clinet_thread([&io_service](){ io_service.run(); });
+    // client
+    boost::asio::io_service client_io;
+    auto client=msgpack::asiorpc::session::create(client_io); 
+    client->connect(boost::asio::ip::tcp::endpoint(
+                    boost::asio::ip::address::from_string("127.0.0.1"), PORT));
+    boost::thread clinet_thread([&client_io](){ client_io.run(); });
 
-        /*
-        std::cout << "call add, 1, 2 = " << client.call(&type_dummy, "add", 1, 2) << std::endl;
-        std::cout << "call add, 3, 4 = " << client.call(std::function<int(int, int)>(), "add", 3, 4) << std::endl;
-        std::cout << "call mul, 5, 7 = " << client.call(&type_dummy, "mul", 5, 7) << std::endl;
-        */
+    // request
+    auto request1=client->call(std::function<int(int, int)>(), "add", 1, 2);
+    auto request2=client->call(std::function<float(float, float)>(), "mul", 1.2f, 5.0f);
 
-		boost::this_thread::sleep( boost::posix_time::milliseconds(1000));
+    std::cout << request1->get_sync<int>() << std::endl;
+    std::cout << request2->get_sync<float>() << std::endl;
 
-        auto request=client->call(&type_dummy, "add", 1, 2);
+    client_io.stop();
+    clinet_thread.join();
 
-        std::cout << request->get_sync<int>() << std::endl;
-
-        io_service.stop();
-        clinet_thread.join();
-    }
-
+    server_io.stop();
+    server_thread.join();
 
     return 0;
 }
