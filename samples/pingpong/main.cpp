e722a45de12e5189690231cb47b37a5285648fed
ousttrue <ousttrue@gmail.com>
2021-08-18 14:01:53 +0900

    pingpong

--- samples/pingpong/main.cpp
+++ samples/pingpong/main.cpp
@@ -1,29 +1,36 @@
-#include <asio/experimental/as_tuple.hpp>
 #include <asio/awaitable.hpp>
 #include <asio/co_spawn.hpp>
 #include <asio/detached.hpp>
+#include <asio/experimental/as_tuple.hpp>
 #include <asio/io_context.hpp>
 #include <asio/ip/tcp.hpp>
+#include <asio/system_timer.hpp>
 #include <asio/use_awaitable.hpp>
+#include <functional>
+#include <future>
 #include <iostream>
+#include <thread>
+using namespace std::chrono;
 
-constexpr auto use_nothrow_awaitable = asio::experimental::as_tuple(asio::use_awaitable);
+constexpr auto use_nothrow_awaitable =
+    asio::experimental::as_tuple(asio::use_awaitable);
 
 class server {
 
-  asio::io_context _io_context;
+  asio::io_context &_context;
   asio::ip::tcp::acceptor _acceptor;
 
 public:
-  server() : _acceptor(_io_context) {}
+  server(asio::io_context &context) : _context(context), _acceptor(context) {}
   ~server() {}
 
   void listen(const asio::ip::tcp::endpoint &ep) {
+    std::cout << "[server]listen: " << ep << "..." << std::endl;
     _acceptor.open(ep.protocol());
     _acceptor.bind(ep);
     _acceptor.listen();
 
-    auto ex = _io_context.get_executor();
+    auto ex = _context.get_executor();
     asio::co_spawn(ex, accept_loop(), asio::detached);
   }
 
@@ -31,22 +38,78 @@ public:
 
     while (true) {
 
-      auto [e, client] =
-          co_await _acceptor.async_accept(use_nothrow_awaitable);
+      auto [e, client] = co_await _acceptor.async_accept(use_nothrow_awaitable);
       if (e) {
         break;
       }
-
-      //   auto ex = client.get_executor();
-      //   co_spawn(ex, proxy(std::move(client), target), detached);
     }
   }
 };
 
+class client {
+
+  asio::ip::tcp::socket _socket;
+
+public:
+  asio::io_context &_context;
+  client(asio::io_context &context)
+      : _socket(context.get_executor()), _context(context) {}
+
+  asio::awaitable<void> connect_async(const asio::ip::tcp::endpoint &ep) {
+    auto [e] = co_await _socket.async_connect(ep, use_nothrow_awaitable);
+  }
+
+  template <typename R>
+  std::future<R> spawn(const std::function<asio::awaitable<R>()> &co) {
+    std::promise<R> p;
+    auto f = p.get_future();
+    auto task = [p = std::move(p), co]() mutable -> asio::awaitable<void> {
+      co_await co();
+      p.set_value();
+    };
+
+    auto ex = _context.get_executor();
+    asio::co_spawn(ex, std::move(task), asio::detached);
+
+    return f;
+  }
+};
+
+const auto PORT = 8070;
+
 int main(int argc, char **argv) {
 
-  std::cout << "start..." << std::endl;
-  std::cout << _MSC_VER << std::endl;
-  std::cout << _MSVC_LANG << std::endl;
+  auto ep = asio::ip::tcp::endpoint(asio::ip::address::from_string("127.0.0.1"),
+                                    PORT);
+
+  // server
+  asio::io_context server_context;
+  server server(server_context);
+  server.listen(ep);
+  std::thread server_thread([&server_context]() { server_context.run(); });
+
+  // client
+  asio::io_context client_context;
+  client client(client_context);
+  auto co = [&client, ep]() -> asio::awaitable<void> {
+    std::cout << "[client]wait 1000ms..." << std::endl;
+    asio::system_timer timer(client._context);
+    timer.expires_from_now(1000ms);
+    co_await timer.async_wait(asio::use_awaitable);
+
+    std::cout << "[client]connect: " << ep << "..." << std::endl;
+    co_await client.connect_async(ep);
+    std::cout << "[client]connected" << std::endl;
+  };
+  auto connected = client.spawn<void>(co);
+  std::thread client_thread([&client_context]() { client_context.run(); });
+  connected.get();
+
+  // stop asio
+  server_context.stop();
+  client_thread.join();
+  server_thread.join();
+  std::cout << "join" << std::endl;
+
   return 0;
 }
--- samples/sample1/main.cpp
+++ samples/sample1/main.cpp
@@ -1,286 +1,246 @@
 #include "asio/io_service.hpp"
+#include <asio.hpp>
+#include <functional>
+#include <iostream>
 #include <iterator>
 #include <memory>
 #include <msgpackpp.h>
-#include <asio.hpp>
 #include <stdexcept>
 #include <stdint.h>
-#include <thread>
-#include <iostream>
 #include <string_view>
-#include <functional>
+#include <thread>
 #include <unordered_map>
 #include <utility>
 
-namespace msgpackpp
-{
-namespace rpc
-{
+namespace msgpackpp {
+namespace rpc {
 using on_message_func = std::function<bytes(const parser &)>;
 using on_response_func = std::function<void(const parser &)>;
 
-class transport : std::enable_shared_from_this<transport>
-{
-    std::shared_ptr<asio::ip::tcp::socket> _socket;
-    std::unordered_map<int, on_response_func> _call_map;
+class transport : std::enable_shared_from_this<transport> {
+  std::shared_ptr<asio::ip::tcp::socket> _socket;
+  std::unordered_map<int, on_response_func> _call_map;
 
-    transport(const on_message_func &callback)
-        : _callback(callback)
-    {
-    }
+  transport(const on_message_func &callback) : _callback(callback) {}
 
 public:
-    ~transport()
-    {
-        auto a = 0;
-    }
+  ~transport() { auto a = 0; }
 
-    on_message_func _callback;
-    std::vector<uint8_t> _buffer;
-    static std::shared_ptr<transport> create(const on_message_func &callback)
-    {
-        return std::shared_ptr<transport>(new transport(callback));
-    }
+  on_message_func _callback;
+  std::vector<uint8_t> _buffer;
+  static std::shared_ptr<transport> create(const on_message_func &callback) {
+    return std::shared_ptr<transport>(new transport(callback));
+  }
 
-    void accept(std::shared_ptr<::asio::ip::tcp::socket> socket)
-    {
-        _socket = socket;
-        // set_connection_status(connection_connected);
-        start_read();
-    }
+  void accept(std::shared_ptr<::asio::ip::tcp::socket> socket) {
+    _socket = socket;
+    // set_connection_status(connection_connected);
+    start_read();
+  }
 
-    void start_read()
-    {
-        if (!_socket)
-        {
-            // closed
-            //assert(false);
-            return;
+  void start_read() {
+    if (!_socket) {
+      // closed
+      // assert(false);
+      return;
+    }
+    auto on_read = [shared =
+                        shared_from_this()](const ::asio::error_code &error,
+                                            size_t bytes_transferred) {
+      if (error && (error != ::asio::error::eof)) {
+        // no more read
+        return;
+      } else {
+        auto parsed = parser(shared->_buffer);
+        while (true) {
+          try {
+            auto current = parsed;
+            // stream message. get next
+            parsed = parsed.next();
+            // msgpack-rpc request [0, id, method, args]
+            auto result = shared->_callback(current);
+            shared->write_async(result);
+          } catch (...) {
+            // no more read
+            // todo: close after write
+            break;
+          }
         }
-        auto on_read = [shared = shared_from_this()](
-                           const ::asio::error_code &error,
-                           size_t bytes_transferred)
-        {
-            if (error && (error != ::asio::error::eof))
-            {
-                // no more read
-                return;
-            }
-            else
-            {
-                auto parsed = parser(shared->_buffer);
-                while (true)
-                {
-                    try
-                    {
-                        auto current = parsed;
-                        // stream message. get next
-                        parsed = parsed.next();
-                        // msgpack-rpc request [0, id, method, args]
-                        auto result = shared->_callback(current);
-                        shared->write_async(result);
-                    }
-                    catch (...)
-                    {
-                        // no more read
-                        // todo: close after write
-                        break;
-                    }
-                }
 
-                // consume used buffer
-                auto d = parsed.data() - shared->_buffer.data();
-                shared->_buffer.erase(shared->_buffer.begin(), shared->_buffer.begin() + d);
+        // consume used buffer
+        auto d = parsed.data() - shared->_buffer.data();
+        shared->_buffer.erase(shared->_buffer.begin(),
+                              shared->_buffer.begin() + d);
 
-                // next loop
-                shared->start_read();
-            }
-        };
-        _socket->async_read_some(
-            asio::buffer(_buffer),
-            on_read);
+        // next loop
+        shared->start_read();
+      }
+    };
+    _socket->async_read_some(asio::buffer(_buffer), on_read);
+  }
+
+  void write_async(const bytes &bytes) {
+    if (!_socket) {
+      assert(false);
+      return;
     }
+    auto on_write = [shared =
+                         shared_from_this()](const ::asio::error_code &error,
+                                             size_t bytes_transferred) {
+      if (error) {
+        throw std::runtime_error("write_async");
+      }
+    };
+    _socket->async_write_some(::asio::buffer(bytes.data(), bytes.size()),
+                              on_write);
+  }
 
-    void write_async(const bytes &bytes)
-    {
-        if (!_socket)
-        {
-            assert(false);
-            return;
-        }
-        auto on_write = [shared = shared_from_this()](
-                            const ::asio::error_code &error,
-                            size_t bytes_transferred)
-        {
-            if (error)
-            {
-                throw std::runtime_error("write_async");
-            }
-        };
-        _socket->async_write_some(
-            ::asio::buffer(bytes.data(), bytes.size()), on_write);
-    }
+  static std::shared_ptr<transport>
+  connect_async(asio::io_service &io_service,
+                const asio::ip::tcp::endpoint &ep) {
+    auto new_connection = transport::create({});
+    auto socket = std::make_shared<::asio::ip::tcp::socket>(io_service);
+    auto on_connect = [t = new_connection, socket /*keep socket*/](
+                          const ::asio::error_code &error) {
+      if (error) {
+        throw std::runtime_error("connect_async");
+      } else {
+        t->start_read();
+      }
+    };
+    socket->async_connect(ep, on_connect);
+    return new_connection;
+  }
 
-    static std::shared_ptr<transport> connect_async(asio::io_service &io_service, const asio::ip::tcp::endpoint &ep)
-    {
-        auto new_connection = transport::create({});
-        auto socket = std::make_shared<::asio::ip::tcp::socket>(io_service);
-        auto on_connect = [t = new_connection, socket /*keep socket*/](
-                              const ::asio::error_code &error)
-        {
-            if (error)
-            {
-                throw std::runtime_error("connect_async");
-            }
-            else
-            {
-                t->start_read();
-            }
-        };
-        socket->async_connect(ep, on_connect);
-        return new_connection;
-    }
+  template <typename R, typename... ARGS>
+  std::future<R> call(const std::string &method, ARGS...) {
+    auto p = std::make_shared<std::promise<R>>();
 
-    template <typename R, typename... ARGS>
-    std::future<R> call(const std::string &method, ARGS...)
-    {
-        auto p = std::make_shared<std::promise<R>>();
+    auto f = p->get_future();
 
-        auto f = p->get_future();
+    // register response callback
+    _call_map.insert(std::make_pair(
+        1, [p](const msgpackpp::parser &msg) { p->set_value({}); }));
 
-        // register response callback
-        _call_map.insert(std::make_pair(1, [p](const msgpackpp::parser &msg)
-                                        { p->set_value({}); }));
+    // send request
 
-        // send request
-
-        return f;
-    }
+    return f;
+  }
 };
 
-class server
-{
-    asio::io_service &_io_service;
-    asio::ip::tcp::acceptor m_acceptor;
+class server {
+  asio::io_service &_io_service;
+  asio::ip::tcp::acceptor m_acceptor;
+  std::list<std::shared_ptr<transport>> _transports;
 
 public:
-    server(asio::io_service &io_service) : _io_service(io_service), m_acceptor(io_service)
-    {
-    }
+  server(asio::io_service &io_service)
+      : _io_service(io_service), m_acceptor(io_service) {}
 
-    void start_accept(const on_message_func &on_receive)
-    {
-        auto new_connection = transport::create(on_receive);
-        auto socket = std::make_shared<asio::ip::tcp::socket>(_io_service);
+  void start_accept(const on_message_func &on_receive) {
+    auto socket = std::make_shared<asio::ip::tcp::socket>(_io_service);
 
-        auto on_accept = [self = this, on_receive, new_connection, socket /*keep socket*/](
-                             const ::asio::error_code &error)
-        {
-            if (error)
-            {
-                throw error;
-            }
-            else
-            {
-                new_connection->accept(socket);
-                // next
-                self->start_accept(on_receive);
-            }
-        };
-        m_acceptor.async_accept(*socket.get(), on_accept);
-    }
+    auto on_accept = [self = this, on_receive,
+                      socket /*keep socket*/](const ::asio::error_code &error) {
+      auto new_connection = transport::create(on_receive);
+      if (error) {
+        throw error;
+      } else {
+        self->_transports.push_back(new_connection);
+        new_connection->accept(socket);
+        // next
+        self->start_accept(on_receive);
+      }
+    };
+    m_acceptor.async_accept(*socket.get(), on_accept);
+  }
 
-    void listen(const asio::ip::tcp::endpoint &ep, const on_message_func &callback)
-    {
-        m_acceptor.open(ep.protocol());
-        m_acceptor.bind(ep);
-        m_acceptor.listen();
-        start_accept(callback);
-    }
+  void listen(const asio::ip::tcp::endpoint &ep,
+              const on_message_func &callback) {
+    m_acceptor.open(ep.protocol());
+    m_acceptor.bind(ep);
+    m_acceptor.listen();
+    start_accept(callback);
+  }
 };
 
-class dispatcher
-{
-    std::unordered_map<std::string, procedurecall> _map;
+class dispatcher {
+  std::unordered_map<std::string, procedurecall> _map;
 
 public:
-    // [0, id, method, args]
-    bytes dispatch(const parser &parsed)
-    {
-        if (parsed[0].get_number<int>() != 0)
-        {
-            throw std::runtime_error("not implemented");
-        }
-
-        auto id = parsed[1].get_number<int>();
-        auto found = _map.find(std::string(parsed[2].get_string()));
-        if (found == _map.end())
-        {
-            throw std::runtime_error("not implemented");
-        }
-
-        auto result = (found->second)(parsed[3]);
-        return make_rpc_response(id, "", result);
+  // [0, id, method, args]
+  bytes dispatch(const parser &parsed) {
+    if (parsed[0].get_number<int>() != 0) {
+      throw std::runtime_error("not implemented");
     }
 
-    template <typename T>
-    void add_handler(const std::string_view name, const T &callback)
-    {
-        _map.insert(std::make_pair(name, make_procedurecall(callback)));
+    auto id = parsed[1].get_number<int>();
+    auto found = _map.find(std::string(parsed[2].get_string()));
+    if (found == _map.end()) {
+      throw std::runtime_error("not implemented");
     }
+
+    auto result = (found->second)(parsed[3]);
+    return make_rpc_response(id, "", result);
+  }
+
+  template <typename T>
+  void add_handler(const std::string_view name, const T &callback) {
+    _map.insert(std::make_pair(name, make_procedurecall(callback)));
+  }
 };
 
 } // namespace rpc
 } // namespace msgpackpp
 
-class SomeClass
-{
-    int m_number;
+class SomeClass {
+  int m_number;
 
 public:
-    void setNumber(const int &number) { m_number = number; }
-    int getNumber() const { return m_number; }
+  void setNumber(const int &number) { m_number = number; }
+  int getNumber() const { return m_number; }
 };
 
-int main(int argc, char **argv)
-{
-    const static int PORT = 8070;
+int main(int argc, char **argv) {
+  const static int PORT = 8070;
 
-    // dispatcher
-    msgpackpp::rpc::dispatcher dispatcher;
-    dispatcher.add_handler("add", [](int a, int b) -> int
-                           { return a + b; });
-    dispatcher.add_handler("mul", [](float a, float b) -> float
-                           { return a * b; });
-    SomeClass s;
-    std::function<int()> getter = std::bind(&SomeClass::getNumber, &s);
-    std::function<void(int)> setter = std::bind(&SomeClass::setNumber, &s, std::placeholders::_1);
+  // dispatcher
+  msgpackpp::rpc::dispatcher dispatcher;
+  dispatcher.add_handler("add", [](int a, int b) -> int { return a + b; });
+  dispatcher.add_handler("mul",
+                         [](float a, float b) -> float { return a * b; });
+  SomeClass s;
+  std::function<int()> getter = std::bind(&SomeClass::getNumber, &s);
+  std::function<void(int)> setter =
+      std::bind(&SomeClass::setNumber, &s, std::placeholders::_1);
 
-    // server
-    asio::io_service server_io;
-    msgpackpp::rpc::server server(server_io);
-    server.listen(asio::ip::tcp::endpoint(asio::ip::tcp::v4(), PORT), [&dispatcher](const msgpackpp::parser &msg) -> msgpackpp::bytes
-                  { return dispatcher.dispatch(msg); });
-    std::thread server_thread([&server_io]()
-                              { server_io.run(); });
+  // server
+  asio::io_service server_io;
+  msgpackpp::rpc::server server(server_io);
+  server.listen(
+      asio::ip::tcp::endpoint(asio::ip::tcp::v4(), PORT),
+      [&dispatcher](const msgpackpp::parser &msg) -> msgpackpp::bytes {
+        return dispatcher.dispatch(msg);
+      });
+  std::thread server_thread([&server_io]() { server_io.run(); });
 
-    // client
-    asio::io_service client_io;
-    auto client = msgpackpp::rpc::transport::connect_async(client_io, asio::ip::tcp::endpoint(
-                                                                          asio::ip::address::from_string("127.0.0.1"), PORT));
-    std::thread clinet_thread([&client_io]()
-                              { client_io.run(); });
+  // client
+  asio::io_service client_io;
+  auto client = msgpackpp::rpc::transport::connect_async(
+      client_io, asio::ip::tcp::endpoint(
+                     asio::ip::address::from_string("127.0.0.1"), PORT));
+  std::thread clinet_thread([&client_io]() { client_io.run(); });
 
-    // request
-    auto result = client->call<int>("add", 1, 2);
-    std::cout << "add, 1, 2 = " << result.get() << std::endl;
+  // request
+  auto result = client->call<int>("add", 1, 2);
+  std::cout << "add, 1, 2 = " << result.get() << std::endl;
 
-    // stop asio
-    client_io.stop();
-    clinet_thread.join();
+  // stop asio
+  client_io.stop();
+  clinet_thread.join();
 
-    server_io.stop();
-    server_thread.join();
+  server_io.stop();
+  server_thread.join();
 
-    return 0;
+  return 0;
 }
