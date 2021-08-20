9141481add8c39d08583a44d8eb78570c2973872
ousttrue <ousttrue@gmail.com>
2021-08-20 23:38:05 +0900

    fix test/call.cpp

--- test/call.cpp
+++ test/call.cpp
@@ -1,112 +1,102 @@
-#include <catch.hpp> 
+#include <catch.hpp>
 
-#include <msgpack/rpc/asio.h>
 #include "fixture.h"
+#include <msgpack_rpc.h>
 
+TEST_CASE("func0") {
+  const static int PORT = 8070;
 
-TEST_CASE( "func0" )
-{
-    const static int PORT=8070;
+  Fixture f(PORT);
 
-    Fixture f(PORT);
+  // client
+  ::asio::io_service client_io;
+  msgpack_rpc::client client(client_io);
+  client.connect_async(::asio::ip::tcp::endpoint(
+      ::asio::ip::address::from_string("127.0.0.1"), PORT));
+  std::thread client_thread([&client_io]() { client_io.run(); });
 
-    // client
-    ::asio::io_service client_io;
-	msgpack_rpc::client client(client_io);
-    client.connect_async(::asio::ip::tcp::endpoint(
-                ::asio::ip::address::from_string("127.0.0.1"), PORT));
-    std::thread client_thread([&client_io](){ client_io.run(); });
+  // request
+  REQUIRE(client.call<int>("zero").get() == 0);
 
-    // request
-    int result;
-    REQUIRE(client.call_sync(&result, "zero")== 0);
-
-    client_io.stop();
-    client_thread.join();
+  client_io.stop();
+  client_thread.join();
 }
 
-TEST_CASE( "func1" )
-{
-    const static int PORT=8070;
+TEST_CASE("func1") {
+  const static int PORT = 8070;
 
-    Fixture f(PORT);
+  Fixture f(PORT);
 
-    // client
-    ::asio::io_service client_io;
-	msgpack_rpc::client client(client_io);
-    client.connect_async(::asio::ip::tcp::endpoint(
-                ::asio::ip::address::from_string("127.0.0.1"), PORT));
-    std::thread client_thread([&client_io](){ client_io.run(); });
+  // client
+  ::asio::io_service client_io;
+  msgpack_rpc::client client(client_io);
+  client.connect_async(::asio::ip::tcp::endpoint(
+      ::asio::ip::address::from_string("127.0.0.1"), PORT));
+  std::thread client_thread([&client_io]() { client_io.run(); });
 
-    // request
-    std::shared_ptr<msgpack_rpc::func_call> req=client.call_async("acc", 1);
-    req->sync();
-    int result;
-    REQUIRE(req->sync().convert(&result)== 1);
+  // request
+  auto req = client.call<int>("acc", 1);
+  req.wait();
+  int result = req.get();
+  REQUIRE(result == 1);
 
-    client_io.stop();
-    client_thread.join();
+  client_io.stop();
+  client_thread.join();
 }
 
-TEST_CASE( "func2" )
-{
-    const static int PORT=8070;
+TEST_CASE("func2") {
+  const static int PORT = 8070;
 
-    Fixture f(PORT);
+  Fixture f(PORT);
 
-    // client
-    ::asio::io_service client_io;
-	msgpack_rpc::client client(client_io);
-    client.connect_async(::asio::ip::tcp::endpoint(
-                ::asio::ip::address::from_string("127.0.0.1"), PORT));
-    std::thread client_thread([&client_io](){ client_io.run(); });
+  // client
+  ::asio::io_service client_io;
+  msgpack_rpc::client client(client_io);
+  client.connect_async(::asio::ip::tcp::endpoint(
+      ::asio::ip::address::from_string("127.0.0.1"), PORT));
+  std::thread client_thread([&client_io]() { client_io.run(); });
 
-    // request
-    int result;
-    REQUIRE(client.call_sync(&result, "add", 3, 4)== 7);
+  // request
+  REQUIRE(client.call<int>("add", 3, 4).get() == 7);
 
-    client_io.stop();
-    client_thread.join();
+  client_io.stop();
+  client_thread.join();
 }
 
-TEST_CASE( "func3" )
-{
-    const static int PORT=8070;
+TEST_CASE("func3") {
+  const static int PORT = 8070;
 
-    Fixture f(PORT);
+  Fixture f(PORT);
 
-    // client
-    ::asio::io_service client_io;
-	msgpack_rpc::client client(client_io);
-    client.connect_async(::asio::ip::tcp::endpoint(
-                ::asio::ip::address::from_string("127.0.0.1"), PORT));
-    std::thread client_thread([&client_io](){ client_io.run(); });
+  // client
+  ::asio::io_service client_io;
+  msgpack_rpc::client client(client_io);
+  client.connect_async(::asio::ip::tcp::endpoint(
+      ::asio::ip::address::from_string("127.0.0.1"), PORT));
+  std::thread client_thread([&client_io]() { client_io.run(); });
 
-    // request
-    int result;
-    REQUIRE(client.call_sync(&result, "add3", 3, 4, 5)== 12);
+  // request
+  REQUIRE(client.call<int>("add3", 3, 4, 5).get() == 12);
 
-    client_io.stop();
-    client_thread.join();
+  client_io.stop();
+  client_thread.join();
 }
 
-TEST_CASE( "func4" )
-{
-    const static int PORT=8070;
+TEST_CASE("func4") {
+  const static int PORT = 8070;
 
-    Fixture f(PORT);
+  Fixture f(PORT);
 
-    // client
-    ::asio::io_service client_io;
-	msgpack_rpc::client client(client_io);
-    client.connect_async(::asio::ip::tcp::endpoint(
-                ::asio::ip::address::from_string("127.0.0.1"), PORT));
-    std::thread client_thread([&client_io](){ client_io.run(); });
+  // client
+  ::asio::io_service client_io;
+  msgpack_rpc::client client(client_io);
+  client.connect_async(::asio::ip::tcp::endpoint(
+      ::asio::ip::address::from_string("127.0.0.1"), PORT));
+  std::thread client_thread([&client_io]() { client_io.run(); });
 
-    // request
-    int result;
-    REQUIRE(client.call_sync(&result, "add4", 3, 4, 5, 6)== 18);
+  // request
+  REQUIRE(client.call<int>("add4", 3, 4, 5, 6).get() == 18);
 
-    client_io.stop();
-    client_thread.join();
+  client_io.stop();
+  client_thread.join();
 }
--- test/fixture.h
+++ test/fixture.h
@@ -1,82 +1,71 @@
 #pragma once
 #include <iostream>
+#include <msgpack_rpc.h>
 
+struct Fixture {
+  // dispatcher
+  asio::io_service dispatcher_io;
+  asio::io_service::work work;
+  msgpack_rpc::dispatcher dispatcher;
+  std::shared_ptr<std::thread> dispatcher_thread;
 
-struct Fixture 
-{
-    // dispatcher
-    ::asio::io_service dispatcher_io;
-	::asio::io_service::work work;
-	msgpack_rpc::dispatcher dispatcher;
-    std::shared_ptr<std::thread> dispatcher_thread;
+  asio::io_service server_io;
+  msgpack_rpc::server server;
+  std::shared_ptr<std::thread> server_thread;
+  std::mutex m_mutex;
 
-    ::asio::io_service server_io;
-    msgpack_rpc::server server;
-    std::shared_ptr<std::thread> server_thread;
-    std::mutex m_mutex;
+  Fixture(int port) : server(server_io), work(dispatcher_io) {
+    dispatcher.add_handler("zero", &Fixture::zero);
+    dispatcher.add_handler("acc", &Fixture::unary);
+    dispatcher.add_handler("add", &Fixture::binary);
+    dispatcher.add_handler("add3",
+                           [](int a, int b, int c) { return a + b + c; });
+    dispatcher.add_handler(
+        "add4", [](int a, int b, int c, int d) { return a + b + c + d; });
 
-    Fixture(int port) 
-        : server(server_io), work(dispatcher_io)
-    {
-        dispatcher.add_handler("zero", &Fixture::zero);
-        dispatcher.add_handler("acc", &Fixture::unary);
-        dispatcher.add_handler("add", &Fixture::binary);
-        dispatcher.add_handler("add3", [](int a, int b, int c){ return a+b+c; });
-        dispatcher.add_handler("add4", [](int a, int b, int c, int d){ return a+b+c+d; });
+    auto pDispatcher = &dispatcher;
+    auto pDispatcherIO = &dispatcher_io;
+    auto on_receive = [pDispatcher, pDispatcherIO](
+                          const msgpackpp::bytes &msg,
+                          std::shared_ptr<msgpack_rpc::session> session) {
+      auto self = pDispatcher;
+      pDispatcherIO->post(
+          [self, msg, session]() { self->dispatch(msg, session); });
+    };
+    server.set_on_receive(on_receive);
 
-		auto pDispatcher=&dispatcher;
-		auto pDispatcherIO=&dispatcher_io;
-        auto on_receive=[pDispatcher, pDispatcherIO](
-                const msgpack::object &msg, 
-                std::shared_ptr<msgpack_rpc::session> session)
-        {
-            auto self=pDispatcher;
-            pDispatcherIO->post([self, msg, session](){
-                    self->dispatch(msg, session);
-                    });
-        };
-		server.set_on_receive(on_receive);
+    auto &mutex = m_mutex;
+    auto error_handler = [&mutex](asio::error_code error) {
+      if (error == asio::error::connection_reset) {
+        // closed
+        return;
+      }
+      std::lock_guard<std::mutex> lock(mutex);
+      auto msg = error.message();
+      std::cerr << msg << std::endl;
+    };
+    server.set_error_handler(error_handler);
 
-		auto &mutex=m_mutex;
-        auto error_handler=[&mutex](asio::error_code error)
-        {
-			if(error==::asio::error::connection_reset){
-				// closed
-				return;
-			}
-            std::lock_guard<std::mutex> lock(mutex);
-			auto msg=error.message();
-            std::cerr << msg << std::endl;
-        };
-        server.set_error_handler(error_handler);
+    server.listen(asio::ip::tcp::endpoint(asio::ip::tcp::v4(), port));
+    server_thread = std::make_shared<std::thread>([&] { server_io.run(); });
 
-        server.listen(::asio::ip::tcp::endpoint(::asio::ip::tcp::v4(), port));
-        server_thread=std::make_shared<std::thread>([&]{ server_io.run(); });
+    dispatcher_thread =
+        std::make_shared<std::thread>([&] { dispatcher_io.run(); });
+  }
+  ~Fixture() {
+    dispatcher_io.stop();
+    dispatcher_thread->join();
+    server_io.stop();
+    server_thread->join();
+  }
 
-        dispatcher_thread=std::make_shared<std::thread>([&]{ dispatcher_io.run(); });
-    }   
-    ~Fixture() {
-        dispatcher_io.stop();
-        dispatcher_thread->join();
-        server_io.stop();
-        server_thread->join();
-    }   
+  std::mutex &mutex() { return m_mutex; }
 
-    std::mutex &mutex(){ return m_mutex; }
-
-    static int zero()
-    {
-        return 0;
-    }
-    static int unary(int a)
-    {
-        static int acc=0;
-        acc+=a;
-        return acc;
-    }
-    static int binary(int a, int b)
-    {
-        return a+b;
-    }
+  static int zero() { return 0; }
+  static int unary(int a) {
+    static int acc = 0;
+    acc += a;
+    return acc;
+  }
+  static int binary(int a, int b) { return a + b; }
 };
-
